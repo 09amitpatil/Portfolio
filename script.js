73541ca668f1b6a810f9bed970142e84fdb05cbb
amit <patil09amit@gmail.com>
2025-01-24 09:52:22 +0530

    update

--- script.js
+++ script.js
@@ -5,3 +5,21 @@ document.getElementById('contactForm').addEventListener('submit', function(event
     this.reset(); 
     alert('Message sent successfully!');
 });
+
+const backToTopButton = document.getElementById('backToTop');
+
+window.addEventListener('scroll', () => 
+{
+  if (window.scrollY > 100) {
+    backToTopButton.classList.add('show');
+  } else {
+    backToTopButton.classList.remove('show');
+  }
+});
+
+backToTopButton.addEventListener('click', () => {
+  window.scrollTo({
+    top: 0,
+    behavior: 'smooth',
+  });
+});
