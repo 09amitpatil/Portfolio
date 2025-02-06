531af8dda2d140ddbaa9ed70a1fff2f2ddb427d6
amit <patil09amit@gmail.com>
2025-02-06 18:24:47 +0530

    enhance contact form functionality and update project descriptions

--- script.js
+++ script.js
@@ -1,15 +1,7 @@
 
-
-document.getElementById('contactForm').addEventListener('submit', function(event) {
-    event.preventDefault(); 
-    this.reset(); 
-    alert('Message sent successfully!');
-});
-
 const backToTopButton = document.getElementById('backToTop');
 
-window.addEventListener('scroll', () => 
-{
+window.addEventListener('scroll', () => {
   if (window.scrollY > 100) {
     backToTopButton.classList.add('show');
   } else {
@@ -23,3 +15,24 @@ backToTopButton.addEventListener('click', () => {
     behavior: 'smooth',
   });
 });
+
+
+emailjs.init("5tScuY8XLrEO-I1aI"); // Replace with your EmailJS user ID
+
+document.getElementById("contactForm").addEventListener("submit", function (event) {
+  event.preventDefault(); // Prevent default form submission
+
+  emailjs.send("service_be257t1", "template_6g543ly", {
+    first_name: document.getElementById("first-name").value,
+    last_name: document.getElementById("last-name").value,
+    email: document.getElementById("email").value,
+    phone: document.getElementById("phone-number").value,
+    message: document.getElementById("message").value
+  }).then(function (response) {
+    alert("Your message has been sent successfully!");
+    document.getElementById("contactForm").reset(); // Reset the form
+  }, function (error) {
+    alert("Error sending message. Please try again.");
+  });
+});
+
